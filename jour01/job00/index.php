de6913c634831d59d5fbae7058451f385d90ac35
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-02 10:14:10 +0100

    Commit job00

---
+++ jour01/job00/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    <?php
+
+    echo "Hello La Plateforme"
+
+    ?>
+
+</html>
