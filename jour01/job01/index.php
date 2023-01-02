7dd6540a804327e48f596f828c898360204a6e5c
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-02 10:34:32 +0100

    Commit job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <?php
+
+    $str = "LaPlateforme";
+
+    echo "$str<br>";
+
+    $str2 = "Vive";
+    $str3 = "!";
+
+    echo " $str2 $str $str3<br>";
+
+    $val = 6;
+    echo "Valeur de la variable val: $val <br>";
+
+    $val = $val + 4;
+    echo "Nouvelle valeur de la variable val: $val <br>";
+
+    $myBool = true;
+    echo "Contenu de la variable myBool: $myBool <br>";
+
+    $myBool = !$myBool;
+    echo "Nouveau contenu de la variable myBool: $myBool <br>";
+
+    ?>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    
+
+    <header>
+    
+    </header>
+
+    <body>
+
+    </body>
+
+    <footer>
+
+    </footer>
+
+</html>
