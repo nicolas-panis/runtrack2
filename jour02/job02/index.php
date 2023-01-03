a2eba64310f1b2d5fa0a7f3cfc823879ea4b644c
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-03 10:34:39 +0100

    Commit jour02/job02 Condition avec OU

---
+++ jour02/job02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    <?php
+        for($i = 0; $i <= 1337; $i++){
+            if ($i == 26 || $i == 37 || $i == 88 || $i == 1111 || $i == 3233){
+                
+            }
+            else{
+                echo $i ?> <br /> <?php
+            }
+        }
+    ?>
+</html>
