1401ed094c68e9e2d1ba7ca53d6c49502823cfcc
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-04 15:24:51 +0100

    Job07 terminé

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -10,13 +10,6 @@
         $str = "Certaines choses changent, et d'autres ne changeront jamais.";
         $str2 = "";
 
-        /*while (isset($str[$i])) $i++;
-        
-
-        for ($i = $i-1; $i >= 0; $i--){
-            $str2[$i - 1] = $str[$i];
-        }*/
-
         for ($i = 0; isset($str[$i]); $i++){
             if (isset($str[$i+1]) == FALSE){
                 $str2[$i] = $str[0];
