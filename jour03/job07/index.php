425ffbf8657eb90dc5ee59f65d157656d28f9c40
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-04 12:41:18 +0100

    Job07 - Remplacer une lettre par la lettre suivante dans une chaine de caractère

---
+++ jour03/job07/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    <?php
+        $str = "Certaines choses changent, et d'autres ne changeront jamais.";
+        $str2 = $str;
+
+        $i = 0;
+        while (isset($str[$i])){
+            $i++;
+        }
+
+        $i--;           //On enlève une valeur qui correspond à la fin de la boucle donc qu'il n'y a plus de valeur dans la chaîne
+        $j = $i;
+
+        while($i >= 0){
+            $str2[$j-1] = $str[$i];
+            $i--;
+            $j--;
+        }
+        echo $str2;
+    ?>
+</html>
