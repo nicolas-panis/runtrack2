cdae0dd66c84bd1b0d72558721b095838e632c91
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-05 10:42:47 +0100

    job04 - Afficher arguments post dans un tableau avec leurs valeurs

---
+++ jour04/job04/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    <body>
+
+        <form action="index.php" method="post">
+            <label> Nom: <input type = "text" name = "nom" /> </label>
+            <label> Prénom: <input type = "text" name = "prenom" /> </label>
+            <label> Age: <input type = "text" name = "age" /> </label>
+            <input type = "submit" value = "Envoyer" /><br />
+        </form>
+
+        <table style = "border: 1px solid black">
+
+            <thead>
+                    <tr>
+                        <th style = "border: 1px solid black">Argument</th>
+                        <th style = "border: 1px solid black">Valeur</th>
+                    </tr>
+                </thead>
+
+            <?php ;
+                foreach($_POST as $key => $value){
+                    ?> <tr> <?php
+                    
+                        ?><td style = "border: 1px solid black"> <?php echo $key ?> </td> <?php
+
+                        ?><td style = "border: 1px solid black"> <?php echo $value ?> </td> <?php
+                }
+            ?>
+
+        </table>
+
+    <body>
+
+    
+</html>
