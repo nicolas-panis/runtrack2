d9fbb627f5834d71d2ca4c85e362792845c05cf8
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-10 14:57:56 +0100

    job07 - Suppression commentaires inutiles

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -33,10 +33,10 @@
         function cesar($str, $decalage){
             $tabAlphabMaj = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
             $tabAlphab = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+            
             for ($i = 0; isset($str[$i]); $i++){
                 for ($j = 0; isset(($tabAlphab[$j])); $j++){
                     if ($str[$i] == $tabAlphab[$j]){
-                        //echo $tabAlphab[$j+$decalage];
                         if ($j + $decalage > 25){
                             echo $tabAlphab[$j+$decalage - 26];
                         }
@@ -47,7 +47,6 @@
                 }
                 for ($j = 0; isset(($tabAlphabMaj[$j])); $j++){
                     if ($str[$i] == $tabAlphabMaj[$j]){
-                        //echo $tabAlphab[$j+$decalage];
                         if ($j + $decalage > 25){
                             echo $tabAlphabMaj[$j+$decalage - 26];
                         }
