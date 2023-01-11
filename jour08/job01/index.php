8b373392c1e5a76398a5f8781ac66493d7f94500
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-11 11:10:05 +0100

    job01 -> compteur avec $_SESSION mais décalage de 1 par rapport à l'appui sur le bouton

---
+++ jour08/job01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    <form action="index.php" method="get">
+        <input type = "submit" value = "reset" name = "reset"><br/>
+    </form>
+
+    <?php
+
+    session_start();
+
+    if (!isset($_SESSION["nbvisites"])){
+        $_SESSION["nbvisites"] = 0;
+    }
+        
+    if (isset($_SESSION["nbvisites"])){
+        echo $_SESSION["nbvisites"];
+        $_SESSION["nbvisites"]++;
+        
+    }
+
+    if (isset($_GET["reset"])){
+        unset($_SESSION["nbvisites"]);
+        session_destroy();
+    }
+
+    
+    ?>
+
+</html>
