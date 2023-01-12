59ac5ce326a6800b2921259612433ac33271c4eb
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-12 12:21:19 +0100

    job04 - Formulaire connexion - déconnexion simple

---
+++ jour08/job04/index.php
@@ -0,0 +1,60 @@
+<?php
+
+    session_start();
+    
+
+    if (isset($_GET['connexion'])){
+        if ($_COOKIE['prenoms'] == $_GET['prenom']){
+            echo "Bonjour " . $_GET['prenom'];
+            ?><form action='index.php' method= 'get'>
+                <button type = 'submit' value = 'Envoyer' name = 'deco'>Déconnexion</button>
+            </form><?php
+        }
+        else{
+            ?><form action='index.php' method= 'get'>
+                <input type = 'text' name = 'prenom'>
+                <button type = 'submit' value = 'Envoyer' name = 'connexion'>Connexion</button>
+            </form><?php
+        }
+
+        setcookie('prenoms', $_GET['prenom'], time() + 3600);
+
+        /*if (isset($_COOKIE['prenoms'])){
+            /*foreach($_COOKIE['prenoms'] as $value){
+            echo $value;
+            ?> <br /> <?php
+            }
+        }*/
+        
+    }
+
+    else{
+        ?><form action='index.php' method= 'get'>
+            <input type = 'text' name = 'prenom'>
+            <button type = 'submit' value = 'Envoyer' name = 'connexion'>Connexion</button>
+        </form><?php
+    }
+
+    if (isset($_GET['reset'])){
+        unset($_SESSION['prenom']);
+        session_destroy();
+    }
+    
+?>
+
+
+
+<!--<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index PHP</title>
+    </head>
+
+    <form action="index.php" method="get">
+        <input type = "text" name = "prenom">
+        <button type = "submit" value = "Envoyer" name = "connexion">Connexion</button>
+    </form>
+
+</html>
